5fbb14aad57e5470ed9086d99663573d21b0eccc
RyanRiz <ryangriaky92@gmail.com>
2024-06-01 22:23:57 +0800

    Add multer for multipart/form-data input

--- api/src/routes/route.js
+++ api/src/routes/route.js
@@ -8,8 +8,10 @@ import auth from '../middlewares/auth.js';
 import { userRegisterValidationRules } from '../rules/userRegisterValidation.js';
 import { userLoginValidationRules } from '../rules/userLoginValidation.js';
 import { validate } from '../middlewares/validation.js';
+import multer from 'multer';
 
 const router = express.Router();
+const upload = multer();
 
 router.get('/', auth, (req, res) => {
   res.send('Hello World!');
@@ -23,8 +25,8 @@ router.get('/quiz/:id', auth, quizController.getQuizById);
 router.post('/quiz', auth, quizController.createQuiz);
 
 // Auth routes
-router.post('/register', userRegisterValidationRules(), validate, authController.register);
-router.post('/login',  userLoginValidationRules(), validate, authController.login);
+router.post('/register', upload.none(), userRegisterValidationRules(), validate, authController.register);
+router.post('/login', upload.none(), userLoginValidationRules(), validate, authController.login);
 router.post('/logout', authController.logout);
 
 // Profile routes
