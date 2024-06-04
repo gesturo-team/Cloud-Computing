218c19da5a5778d544d6b5c2de53dcf3a4f11de0
rejarevaldy <rejarevaldy30@gmail.com>
2024-06-04 18:39:05 +0800

    refactor: Update authentication middleware to use authorization header

--- api/src/middlewares/auth.js
+++ api/src/middlewares/auth.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
 
 export default function auth(req, res, next) {
-  const token = req.cookies.token;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({
